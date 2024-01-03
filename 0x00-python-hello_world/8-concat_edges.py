44fee7ce530406c4c3c3a1f72fb74779d4222a57
Eze Joshua <joshuaeze87@gmail.com>
2024-01-03 00:55:24 -0800

    sike

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str_data = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+result_str = str_data[39:67] + " with " + str_data[0:6]
+print(result_str)
